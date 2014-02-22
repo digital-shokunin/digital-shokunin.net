e84c89c63b37507158718b21581f510c309ce446
David Mitchell <david.mitchell@digital-shokunin.net>
2014-02-21 21:25:14 -0500

    Update config to new twitter

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,7 +25,7 @@ MENUITEMS = (('Home', 'http://digital-shokunin.net'),)
 # Social widget
 SOCIAL = (('google+','https://plus.google.com/u/0/112141969866873644503/'),
           ('github', 'http://github.com/digital-shokunin'),
-          ('twitter', 'http://twitter.com/surgeterrix'),
+          ('twitter', 'http://twitter.com/digitalshokunin'),
           ('linkedin', 'http://www.linkedin.com/profile/view?id=51799602'),
           ('lastfm', 'http://lastfm.com/user/surgeterrix'),
           ('email', 'mailto:david.mitchell@digital-shokunin.net'),
@@ -44,7 +44,7 @@ DEFAULT_PAGINATION = 10
 DISQUS_SITENAME = "digitalshokunin"
 GOOGLE_ANALYTICS = "UA-37662566-1"
 GITHUB_URL = 'http://github.com/digital-shokunin/'
-TWITTER_USERNAME = 'surgeterrix'
+TWITTER_USERNAME = 'digitalshokunin'
 
 
 DELETE_OUTPUT_DIRECTORY = False
